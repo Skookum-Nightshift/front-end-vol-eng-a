2cd82f402889ea7447f0230bea655f8f117c7616
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-10-14 23:01:12 -0400

    Links via react-router

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,6 +3,7 @@
 require('./styles.css');
 
 import React from 'react';
+import { Router, Route, Link } from 'react-router'
 var {PropTypes} = React;
 import Navbar from 'Navbar';
 
@@ -11,6 +12,7 @@ class Header extends React.Component {
   render(): ?ReactElement {
     return (
       <header className="Header">
+      	<img src="/public/images/uncc_logo2.png" />
         <Navbar />
       </header>
     );
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,13 +3,15 @@
 require('./styles.css');
 
 import React from 'react';
+import { Router, Route, Link } from 'react-router'
+
 var {PropTypes} = React;
 
 class Navbar extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="Navbar">
-        Navbar
+        <Link to="/">Home</Link>
       </div>
     );
   }
