959da44c281702777fe9666a1ff8d50be495c939
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-10-17 13:42:23 -0400

    Materialize the Navbar

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,7 +3,7 @@
 require('./styles.css');
 
 import React from 'react';
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link } from 'react-router';
 var {PropTypes} = React;
 import Navbar from 'Navbar';
 
@@ -11,10 +11,14 @@ import Navbar from 'Navbar';
 class Header extends React.Component {
   render(): ?ReactElement {
     return (
-      <header className="Header">
-      	<img src="/public/images/uncc_logo2.png" />
-        <Navbar />
-      </header>
+    	<nav>
+		    <div className="Header nav-wrapper">
+		      <a href="#" className="brand-logo"><img src="/public/images/uncc_logo2.png" /></a>
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		      	<Navbar />
+		      </ul>
+		    </div>
+		  </nav>
     );
   }
 }
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,7 +3,7 @@
 require('./styles.css');
 
 import React from 'react';
-import { Router, Route, Link } from 'react-router'
+import { Router, Route, Link } from 'react-router';
 
 var {PropTypes} = React;
 
@@ -11,11 +11,11 @@ class Navbar extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="Navbar">
-        <Link to="/">Home</Link>
-        <Link to="/quiz">Take the Quiz</Link>
-        <Link to="/categories">Categories</Link>
-        <Link to="/organizations">Organizations</Link>
-        <Link to="/account">My Account</Link>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/quiz">Take the Quiz</Link></li>
+        <li><Link to="/categories">Categories</Link></li>
+        <li><Link to="/organizations">Organizations</Link></li>
+        <li><Link to="/account">My Account</Link></li>
       </div>
     );
   }
