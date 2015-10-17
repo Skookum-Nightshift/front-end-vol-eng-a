a6ba7349e75511b899a492c66b1e1876aa79d6e1
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-10-16 22:00:42 -0400

    Update Jumbotron for reuse

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -9,8 +9,8 @@ class Jumbotron extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="Jumbotron">
-        <h1>Who we are</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac lectus et mauris efficitur feugiat aliquam in odio. Sed ut congue neque. Donec a erat mauris. Nulla pharetra lobortis mollis. Praesent eu semper tellus, vitae ullamcorper turpis. Proin ut justo lectus. Donec bibendum turpis lectus, non dictum erat laoreet a. Etiam eu lacinia elit. Ut dignissim urna metus, lobortis dignissim nibh bibendum non. Morbi vestibulum iaculis arcu eu rhoncus. Cras iaculis justo consequat, volutpat purus eget, tempus nisl. Proin eu nisi et leo cursus pellentesque.</p>
+        <h1>{this.props.header}</h1>
+        <p>{this.props.paragraph}</p>
       </div>
     );
   }
--- handlers/Home/index.js
+++ handlers/Home/index.js
@@ -7,10 +7,13 @@ import Button from 'Button';
 class Home extends React.Component {
 
   render(): ?ReactElement {
+
+    var paragraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac lectus et mauris efficitur feugiat aliquam in odio. Sed ut congue neque. Donec a erat mauris. Nulla pharetra lobortis mollis. Praesent eu semper tellus, vitae ullamcorper turpis. Proin ut justo lectus. Donec bibendum turpis lectus, non dictum erat laoreet a. Etiam eu lacinia elit. Ut dignissim urna metus, lobortis dignissim nibh bibendum non. Morbi vestibulum iaculis arcu eu rhoncus. Cras iaculis justo consequat, volutpat purus eget, tempus nisl. Proin eu nisi et leo cursus pellentesque."; 
+
     return (
       <div className="Home">
-      	<Jumbotron />
-      	<Carousel />
+      	<Jumbotron header="Who we are" paragraph={paragraph} />
+      	<Carousel type="icons"/>
       	<Button children="Take the Quiz" />
       </div>
     );
