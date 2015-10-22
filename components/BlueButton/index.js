d986f55ff610c8709c7e477b68e6e248ecd6c6c9
mrsdohpaz <rosemary.reilman@gmail.com>
2015-10-22 09:07:22 -0400

    Blue/Green Button Theme components for easier use sitewide

---
+++ components/BlueButton/index.js
@@ -0,0 +1,31 @@
+/** @flow */
+
+require('./styles.css');
+
+import React from 'react';
+var {PropTypes} = React;
+import RaisedButton from 'material-ui/lib/raised-button';
+
+
+class BlueButton extends React.Component {
+  render(): ?ReactElement {
+
+    var {children, type, ...props} = this.props;
+
+    return (
+      <RaisedButton {...props} />
+    );
+  }
+}
+
+BlueButton.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+BlueButton.defaultProps = {
+  label: 'Submit',
+  backgroundColor: '#2196F3',
+  labelColor: '#FFFFFF',
+};
+
+export default BlueButton;
---
+++ components/GreenButton/index.js
@@ -0,0 +1,31 @@
+/** @flow */
+
+require('./styles.css');
+
+import React from 'react';
+var {PropTypes} = React;
+import RaisedButton from 'material-ui/lib/raised-button';
+
+
+class GreenButton extends React.Component {
+  render(): ?ReactElement {
+
+    var {children, type, ...props} = this.props;
+
+    return (
+      <RaisedButton {...props} />
+    );
+  }
+}
+
+GreenButton.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+GreenButton.defaultProps = {
+  label: 'Submit',
+  primary: {true},
+  labelColor: '#FFFFFF',
+};
+
+export default GreenButton;
