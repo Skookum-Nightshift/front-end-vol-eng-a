95319e629574ea21b9b9c1e9fe566f721a533c7e
johnlyden <john.lyden86@gmail.com>
2015-10-28 16:46:02 -0400

    adding categories header centered

--- components/CategoriesHeader/index.js
+++ components/CategoriesHeader/index.js
@@ -10,7 +10,10 @@ class CategoriesHeader extends React.Component {
     return (
       <div className="CategoriesHeader">
         <h2>Categories of Interest</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut mi a odio viverra venenatis vel eget urna. Aliquam non elit vulputate, rutrum arcu vitae, consectetur turpis. Praesent pellentesque iaculis condimentum. Nunc quis fringilla ex, eu sodales ipsum. Nam molestie, dui quis maximus sagittis, turpis nisl molestie ex, quis maximus urna est non nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras convallis ante ut felis dignissim congue. Donec a purus porta, maximus metus accumsan, dictum arcu. Mauris semper, quam eu auctor tempor, tortor tellus facilisis lorem, a aliquam mi magna quis arcu. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut mi a odio viverra venenatis vel eget urna. Aliquam non elit vulputate, rutrum arcu vitae, consectetur turpis. Praesent pellentesque iaculis condimentum.
+        </p>
+        <p>Nunc quis fringilla ex, eu sodales ipsum. Nam molestie, dui quis maximus sagittis, turpis nisl molestie ex, quis maximus urna est non nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras convallis ante ut felis dignissim congue. Donec a purus porta, maximus metus accumsan, dictum arcu. Mauris semper, quam eu auctor tempor, tortor tellus facilisis lorem, a aliquam mi magna quis arcu.
+        </p>
       </div>
     );
   }
