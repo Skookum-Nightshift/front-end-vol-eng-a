4400ef874036956788dc495e7f604bb58819b188
johnlyden <john.lyden86@gmail.com>
2015-10-28 17:03:44 -0400

    adding constructor to the categoriescontainer component to draw in categories

--- components/CategoriesContainer/index.js
+++ components/CategoriesContainer/index.js
@@ -8,6 +8,13 @@ import CategoriesHeader from 'CategoriesHeader';
 import CategoryList from 'CategoryList';
 
 class CategoriesContainer extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+
   render(): ?ReactElement {
     return (
       <div className="CategoriesContainer">
