a8a8995c31530a5ae3c63aba4e09ba18019a7880
johnlyden <john.lyden86@gmail.com>
2015-10-30 15:59:13 -0400

    making category header h1

--- components/CategoriesHeader/index.js
+++ components/CategoriesHeader/index.js
@@ -9,7 +9,7 @@ class CategoriesHeader extends React.Component {
   render(): ?ReactElement {
     return (
       <div className="CategoriesHeader">
-        <h2>Categories</h2>
+        <h1>Categories</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut mi a odio viverra venenatis vel eget urna. Aliquam non elit vulputate, rutrum arcu vitae, consectetur turpis. Praesent pellentesque iaculis condimentum. Nunc quis fringilla ex, eu sodales ipsum. Nam molestie, dui quis maximus sagittis, turpis nisl molestie ex, quis maximus urna est non nisi. Interdum et malesuada fames ac ante ipsum primis in faucibus.
         </p>
       </div>
