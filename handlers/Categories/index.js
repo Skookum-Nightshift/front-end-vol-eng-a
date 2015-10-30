285f9543b09a40e1a89df77f928aa1d63d572a7a
johnlyden <john.lyden86@gmail.com>
2015-10-30 12:50:07 -0400

    responsive 3 column category page

--- handlers/Categories/index.js
+++ handlers/Categories/index.js
@@ -11,7 +11,7 @@ class Categories extends React.Component {
     super();
 
     this.state = {
-      categories: ['1', '2', '3', '4']
+      categories: ['1', '2', '3', '4', '5', '6', '7', '8']
     }
 
   }
