052a1bd7a516320ddfb4b449f0545549329be19d
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-10-30 22:07:33 -0400

    Fix on handler for Categories from merge

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ const {
 
 import NotFound from './handlers/NotFound';
 import App from './handlers/Base';
+import Categories from './handlers/Categories';
 import Home from'./handlers/Home';
 import TakeQuiz from'./handlers/TakeQuiz';
 import QuizResults from'./handlers/QuizResults';
@@ -18,7 +19,7 @@ var routes = (
   <Route path="/" handler={App} >
     <DefaultRoute name="home" handler={Home} />
     <Route name="about" handler={NotFound} />
-    <Route name="categories" handler={NotFound} />
+    <Route name="categories" handler={Categories} />
     <Route name="organizations" handler={NotFound} />
     <Route name="profile" handler={NotFound} />
     <Route name="quiz" handler={TakeQuiz} />
