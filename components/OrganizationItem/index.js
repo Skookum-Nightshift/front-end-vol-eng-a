44f6bd6ffa2a002964e3eeb8b7811cc90c2f3d7f
johnlyden <john.lyden86@gmail.com>
2015-10-31 15:52:03 -0400

    rendering all information for organization item

--- components/OrganizationItem/index.js
+++ components/OrganizationItem/index.js
@@ -12,7 +12,13 @@ class OrganizationItem extends React.Component {
 
     return (
       <div className="OrganizationItem">
+        <img src={this.props.data.icon} width="75"/>
         <h3>{this.props.data.name}</h3>
+        <h4>{this.props.data.contact.address}</h4>
+        <h4>{this.props.data.contact.city}</h4>
+        <h4>{this.props.data.contact.state}</h4>
+        <h4>{this.props.data.contact.zip}</h4>
+        <h4>{this.props.data.contact.web}</h4>
       </div>
     );
   }
