657b04848dab3ffbe724e74b896c1848b3fcf529
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-10-30 22:32:43 -0400

    Update Navbar Title for Bolding

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,16 +6,24 @@ import React from 'react';
 import { Router, Route, Link } from 'react-router';
 var {PropTypes} = React;
 import AppBar from 'material-ui/lib/app-bar';
+import EnhancedButton from 'material-ui/lib/enhanced-button';
 import Navbar from 'Navbar';
 
 class Header extends React.Component {
 
   render(): ?ReactElement {
+
+  	var NavTitle = (
+  			<EnhancedButton className="NavTitle" linkButton={true} href="/" style={{fontSize: 34, paddingTop: 8 }}>
+        	<strong>UWCC</strong>aspires
+      </EnhancedButton>
+  	);
+
     return (
 		    <div className="Header">
 		      <AppBar
 		      	className="AppBar"
-					  title="UWCCaspires"
+					  title={NavTitle}
 					  showMenuIconButton={false}
 					  iconElementRight={ <Navbar /> } />
 				</div>
