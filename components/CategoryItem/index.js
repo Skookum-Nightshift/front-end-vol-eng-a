f7725ae45f9f53f3ace3e6cd826c37dbe2d1c9a1
johnlyden <john.lyden86@gmail.com>
2015-11-03 14:29:41 -0500

    adding dynamic links for each catgory on category show page

--- components/CategoryItem/index.js
+++ components/CategoryItem/index.js
@@ -4,8 +4,13 @@ require('./styles.css');
 
 import React from 'react';
 var {PropTypes} = React;
+import { Router, Route, Link } from 'react-router'
 
 class CategoryItem extends React.Component {
+
+handleClick(event) {
+  console.log("hey");
+}
 
   render(): ?ReactElement {
 
@@ -14,7 +19,8 @@ class CategoryItem extends React.Component {
     return (
       <div className="CategoryItem">
           <img src={this.props.data.icon} width="75"/>
-          <h3>{this.props.data.name}</h3>
+          <h2><Link to={`/category/${this.props.data.id}`}>{this.props.data.name}</Link></h2>
+          <h3>{this.props.data.id}</h3>
       </div>
     );
   }
