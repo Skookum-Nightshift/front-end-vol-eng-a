c2ac1cf6276c1d936c21e922d0bb8e77f8bd8420
KileyDowling <kileydowling@gmail.com>
2015-11-02 20:09:42 -0500

    List is now working

--- components/SingleMatch/index.js
+++ components/SingleMatch/index.js
@@ -3,28 +3,22 @@
 require('./styles.css');
 
 import React from 'react';
+import {List, ListItem, ListDivider, Avatar } from 'material-ui';
 var {PropTypes} = React;
-import List from 'material-ui/lib/lists';
-import ListItem from 'material-ui/lib/lists';
-import Avatar from 'material-ui/lib/lists';
-import leftAvatar from 'material-ui/lib/lists';
-
 
 
 class SingleMatch extends React.Component {
 
   render(): ?ReactElement {
 
-  var {children, type, ...props} = this.props;
-
     return (
       <div className="SingleMatch">
-          <List subheader="Matches">
+          <List className="SingleMatchList">
             <ListItem
-              leftAvatar={<Avatar src={this.props.data.logo} />}
+            leftAvatar={<Avatar src={this.props.data.logo} />}
               primaryText={this.props.data.name}
               secondaryText= {<p>
-              {this.props.data.opportunityName}
+              {this.props.data.opportunityName} <br/>
               {this.props.data.opportunityDescription} 
               </p>}
          secondaryTextLines={2} />
--- handlers/QuizResults/index.js
+++ handlers/QuizResults/index.js
@@ -14,12 +14,23 @@ class QuizResults extends React.Component {
     this.state = {
          results: [
         {
-          name: "Big Brothers & Big Sisters",
+          name: "A Child’s Place",
           logo: "/public/images/help-icon.svg",
-          opportunityName: "Big",
-          opportunityDescription: "As a big, you will..."
+          opportunityName: "Snack Packs",
+          opportunityDescription: "Compile snack bags for children. Each snack pack will include 3 items: juice (prefer 100% juice boxes, but capri suns are also great), carb (crackers, granola, bars, breakfast bars, etc.), fruit (fruit cup, applesauce, fruit snacks). Bags should be lunch size brown bags."
+        },
+             {
+          name: "Big Brothers/Big Sisters",
+          logo: "/public/images/help-icon.svg",
+          opportunityName: "Big Brother or Big Sister",
+          opportunityDescription: "For a few hours, a couple times a month, you can give a Little the invaluable gift of your friendship. By simply changing their perspective of what the world can offer, you can literally start changing their lives. And sometimes it’s as simple as playing video games, going to a museum, or just hanging out together."
+        },             
+        {
+          name: "The Relatives",
+          logo: "/public/images/help-icon.svg",
+          opportunityName: "Dinner with The Relatives",
+          opportunityDescription: "Provide dinner for the youth and staff at the Crisis Center. There are 9 children and 2 staff. You can either drop off dinner, or stay and have dinner with the group."
         }
-
         ]
       };
     }
