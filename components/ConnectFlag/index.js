3f45469ac2f2f83f4e25c8c85eef63dddb0507e7
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-11-08 22:06:43 -0500

    Working on Connect Form

--- components/ConnectFlag/index.js
+++ components/ConnectFlag/index.js
@@ -3,6 +3,7 @@
 require('./styles.css');
 
 import React from 'react';
+import Input from 'Input';
 import Paper from 'material-ui/lib/paper';
 
 var {PropTypes} = React;
@@ -23,10 +24,22 @@ class ConnectFlag extends React.Component {
          		style={{
          				margin:'0 auto', 
          				padding: padding, 
-         				width: '75%', 
+         				width: '85%', 
          				maxHeight: this.props.flagMaxHeight }} 
          		key={0}>
-            { this.props.flagMaxHeight > 0 ? "Connect Form" : null }
+            { this.props.flagMaxHeight > 0 ? 
+
+              <form>
+                <h2>Create a Profile</h2>
+                <Input placeholder="First Name" style={{ width: '40%', marginRight: '5px' }} />
+                <Input placeholder="Last Name" style={{ width: '40%' }} />
+                <Input placeholder="Email" style={{ width: '90%' }} />
+                <Input type="password" placeholder="Password" style={{ width: '90%' }} />
+                <Input type="password" placeholder="Confirm Password" style={{ width: '90%' }} />
+                <Input placeholder="Zip Code" style={{ width: '40%' }} />
+              </form>
+
+             : null }
           </Paper>
       </div>
     );
