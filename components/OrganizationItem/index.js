868848812eaf36fbb3b87c05f2cf3d5064ca64e0
johnlyden <john.lyden86@gmail.com>
2015-11-11 16:34:48 -0500

    removing css from organiation item and adding material-ui card

--- components/OrganizationItem/index.js
+++ components/OrganizationItem/index.js
@@ -4,6 +4,8 @@ require('./styles.css');
 
 import React from 'react';
 var {PropTypes} = React;
+import {Paper} from 'material-ui';
+import {Card} from 'material-ui/lib/card'
 
 class OrganizationItem extends React.Component {
   render(): ?ReactElement {
@@ -12,15 +14,17 @@ class OrganizationItem extends React.Component {
 
     return (
       <div className="OrganizationItem">
-        <div className="comment-image">
-          <img src={this.props.data.icon} width="75"/>
-        </div>
-        <div className="comment-content">
-          <h1>{this.props.data.name}</h1>
-          <p>{this.props.data.address}</p>
-          <p>{this.props.data.city}, {this.props.data.state} {this.props.data.zip}</p>
-          <p>{this.props.data.web}</p>
-        </div>
+        <Card>
+          <div className="comment-image">
+            <img src={this.props.data.icon} width="75"/>
+          </div>
+          <div className="comment-content">
+            <h1>{this.props.data.name}</h1>
+            <p>{this.props.data.address}</p>
+            <p>{this.props.data.city}, {this.props.data.state} {this.props.data.zip}</p>
+            <p>{this.props.data.web}</p>
+          </div>
+        </Card>
       </div>
     );
   }
