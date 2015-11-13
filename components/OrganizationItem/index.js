5adfb05605906dccebe6f06536962dc60ef05f5d
johnlyden <john.lyden86@gmail.com>
2015-11-12 19:00:45 -0500

    adding Link to for each organization item on the org name

--- components/OrganizationItem/index.js
+++ components/OrganizationItem/index.js
@@ -5,7 +5,9 @@ require('./styles.css');
 import React from 'react';
 var {PropTypes} = React;
 import {Paper} from 'material-ui';
-import {Card} from 'material-ui/lib/card'
+import {Card} from 'material-ui/lib/card';
+import {CardActions} from 'material-ui/lib/card';
+import { Router, Route, Link } from 'react-router';
 
 class OrganizationItem extends React.Component {
   render(): ?ReactElement {
@@ -19,7 +21,7 @@ class OrganizationItem extends React.Component {
             <img src={this.props.data.icon} width="75"/>
           </div>
           <div className="comment-content">
-            <h1>{this.props.data.name}</h1>
+            <h1><Link to={`/organization/${this.props.data.id}`}>{this.props.data.name}</Link></h1>
             <p>{this.props.data.address}</p>
             <p>{this.props.data.city}, {this.props.data.state} {this.props.data.zip}</p>
             <p>{this.props.data.web}</p>
