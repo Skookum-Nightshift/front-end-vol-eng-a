06987cea5eb93cdbf22ed598450f35299f060290
johnlyden <john.lyden86@gmail.com>
2015-11-12 19:13:32 -0500

    adding working apiGet request for organization handler

--- handlers/Organization/index.js
+++ handlers/Organization/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import {Resolver} from 'react-resolver';
+import {apiGet} from 'requestLib';
 
 class Organization extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+
+  }
+
+  componentDidMount(){
+    var id = this.context.router.getCurrentParams().id;
+    apiGet(`v1/organizations/${id}`, {},
+      (data) => {
+        console.log(data);
+        this.setState({
+          organization: data.organization
+        });
+      },
+      () => {
+        console.log('error');
+      }
+    );
+  }
+
   render(): ?ReactElement {
     return (
       <div className="Organization">
