5afbd0c6129324c1655fa0c329fab705c9cce85e
KileyDowling <kileydowling@gmail.com>
2015-11-13 19:05:08 -0500

    updating takequiz

--- components/Jumbotron/index.js
+++ components/Jumbotron/index.js
@@ -8,13 +8,17 @@ var {PropTypes} = React;
 class Jumbotron extends React.Component {
   render(): ?ReactElement {
     return (
-      <div className="Jumbotron" style={{ background: 'url(' + this.props.image + ') no-repeat top center', backgroundSize: 'cover' }}>
+      <div className="Jumbotron" style={{ background: 'url(' + this.props.image + ') no-repeat top center', backgroundSize: this.props.size }}>
         <h1>{this.props.header}</h1>
         <p>{this.props.paragraph}</p>
       </div>
     );
   }
 }
+Jumbotron.defaultProps = {
+size: 'cover',
+
+};
 
 Jumbotron.propTypes = {
   id: PropTypes.any.isRequired,
