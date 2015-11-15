e6abee61f383f72a71981d15a75f999561b6feff
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-11-15 10:56:11 -0500

    use userstore to save connections

--- components/ConnectFlag/index.js
+++ components/ConnectFlag/index.js
@@ -7,28 +7,55 @@ import Input from 'Input';
 import Paper from 'material-ui/lib/paper';
 import SubmitButton from 'BlueButton';
 import FlatButton from 'material-ui/lib/flat-button';
+import UserActions from '../../actions/UserActions';
+import UserStore from '../../stores/UserStore';
 import {apiPost} from 'requestLib';
 
 var {PropTypes} = React;
 
 class ConnectFlag extends React.Component {
 
+  constructor(){
+    super();
+
+    this.state = {
+      userData: UserStore.getState().user
+    };
+  }
+
+  componentDidMount() {
+    UserStore.listen((state) => this.setState({ userData: state.userData }));
+  }
+
   sendUserData() {
+
+    if (this.state.userData) {
+      var opportunities = this.state.userData.opportunities; 
+      var organizations = this.state.userData.organizations;
+    } else {
+      var opportunities = new Array(); 
+      var organizations = new Array();
+    }
+
+    opportunities.push(this.props.data.opportunity_id)
+    organizations.push(this.props.data.organization_id);
 
      var userData = {
       "firstname": document.getElementById('firstname').value,
       "lastname": document.getElementById('lastname').value,
       "email": document.getElementById('email').value,
       "zipcode": document.getElementById('zipcode').value,
-      "opportunity_id": this.props.data.opportunity_id,
-      "organization_id": this.props.data.organization.organization_id,
+      "opportunity": this.props.data.opportunity_id,
+      "organization": this.props.data.organization.organization_id,
+      "opportunities": opportunities,
+      "organizations": organizations,
      };
 
     //  apiPost(`/v1/connection`, userData,
     //   (data) => {
 
           this.props.onChange(); 
-
+          UserActions.updateUser(userData);
 
     //   },
     //   () => {
@@ -78,10 +105,11 @@ class ConnectFlag extends React.Component {
 
                   <div className="ConnectForm">
                     <h2>Send Your Information</h2>
-                      <Input id="firstname" placeholder="First Name" value="" style={{ width: '40%', marginRight: '5px' }} />
-                      <Input id="lastname" placeholder="Last Name" style={{ width: '40%' }} />
-                      <Input id="email" placeholder="Email" style={{ width: '90%' }} />
-                      <Input id="zipcode" placeholder="Zip Code" style={{ width: '90%' }} />
+                      <Input 
+                        id="firstname" defaultValue={this.state.userData ? this.state.userData.firstname : null} placeholder="First Name" value="" style={{ width: '40%', marginRight: '5px' }} />
+                      <Input id="lastname" defaultValue={this.state.userData ? this.state.userData.lastname : null} placeholder="Last Name" style={{ width: '40%' }} />
+                      <Input id="email" defaultValue={this.state.userData ? this.state.userData.email : null} placeholder="Email" style={{ width: '90%' }} />
+                      <Input id="zipcode" defaultValue={this.state.userData ? this.state.userData.zipcode : null} placeholder="Zip Code" style={{ width: '90%' }} />
 
                       <div className="clearfix">
                       
