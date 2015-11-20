0f49c5f9a7506d251f1e9fd50cedc6e2b17d2f26
Rosemary Stanley <rosemary.reilman@gmail.com>
2015-11-19 22:11:22 -0500

    Add in Social Links upon connection

--- components/OrganizationPage/index.js
+++ components/OrganizationPage/index.js
@@ -8,11 +8,12 @@ var {PropTypes} = React;
 import Jumbotron from 'Jumbotron';
 import OpportunityItem from 'OpportunityItem';
 import Logo from 'OrganizationLogo';
+import UserStore from '../../stores/UserStore';
 
 class OrganizationPage extends React.Component {
 
-  render(): ?ReactElement {
 
+  render(): ?ReactElement {
     var organization = this.props.organization;
     var addy = <p>{organization.address}, {organization.city}, {organization.state} {organization.zip}</p>;
     var opps = organization.opportunities.map(opp =>
@@ -35,6 +36,14 @@ class OrganizationPage extends React.Component {
               <h1>{organization.name}</h1>
               <p>{organization.address ? addy : "Locations Vary"}</p>
               <p>{organization.description}</p>
+
+              {this.props.connection && organization.web ? <a className="SocialLinks" href={organization.web} target="_blank">Website</a> : null }
+              {this.props.connection && organization.facebook ? <a className="SocialLinks" href={organization.facebook} target="_blank">Facebook</a> : null }
+              {this.props.connection && organization.twitter ? <a className="SocialLinks" href={organization.twitter} target="_blank">Twitter</a> : null }
+              {this.props.connection && organization.instagram ? <a className="SocialLinks" href={organization.instagram} target="_blank">Instagram</a> : null }
+              {this.props.connection && organization.youtube ? <a className="SocialLinks" href={organization.youtube} target="_blank">Youtube</a> : null }
+              {this.props.connection && organization.linkedin ? <a className="SocialLinks" href={organization.linkedin} target="_blank">LinkedIn</a> : null }
+
             </div>
         </div>
       :
